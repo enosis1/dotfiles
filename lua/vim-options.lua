c90ff5ed420472bb622ef5daa0cde577981aeb4f
Mike Johnson <72291166+enosis1@users.noreply.github.com>
2024-02-25 15:19:56 -0500

    remove neoscroll

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,9 +3,10 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 
-vim.keymap.set("n", "<C-d>", "<C-d>zz", {desc = "Center cursor after moving down half-page"})
-vim.keymap.set("n", "<C-u>", "<C-u>zz", {desc = "Center cursor after moving down half-page"})
 vim.g.mapleader = " "
 
 vim.wo.number = true
 vim.wo.relativenumber = true
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
