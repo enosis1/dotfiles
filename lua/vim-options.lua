e5d1d8d562b1c38a4240062480ed7e4e7a549edc
Mike Johnson <72291166+enosis1@users.noreply.github.com>
2024-02-25 15:08:59 -0500

    add neoscroll plugin

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -2,6 +2,8 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz", {desc = "Center cursor after moving down half-page"})
 vim.g.mapleader = " "
 
 vim.wo.number = true
