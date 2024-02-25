018d6204b61293aa14683e38787a1a63610549fd
Mike Johnson <72291166+enosis1@users.noreply.github.com>
2024-02-25 15:11:48 -0500

    remap ^u/^d to always center

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,6 +4,7 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz", {desc = "Center cursor after moving down half-page"})
+vim.keymap.set("n", "<C-u>", "<C-u>zz", {desc = "Center cursor after moving down half-page"})
 vim.g.mapleader = " "
 
 vim.wo.number = true
