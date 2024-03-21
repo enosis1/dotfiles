df29f12573e4ccafc161a481fa58e3e9f5ed8f16
Mike Johnson <72291166+enosis1@users.noreply.github.com>
2024-03-21 17:54:58 -0400

    remove transparency

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -3,9 +3,6 @@ return {
 	name = "catppuccin",
 	priority = 1000,
 	config = function()
-    require("catppuccin").setup({
-      transparent_background = true
-    })
 		vim.cmd.colorscheme("catppuccin-frappe")
 	end,
 }
