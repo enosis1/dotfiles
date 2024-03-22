4e183a56c03141197a083ecc8565c3cc11077210
Mike Johnson <72291166+enosis1@users.noreply.github.com>
2024-03-21 20:50:46 -0400

    add telescope plugins

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,17 @@
 return {
 	{
 		"nvim-telescope/telescope-ui-select.nvim",
+		-- This is your opts table
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
+			require("telescope").load_extension("ui-select")
+		end,
 	},
 	{
 		"nvim-telescope/telescope.nvim",
