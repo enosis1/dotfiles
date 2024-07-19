19b2b5349a2c6d31435c62b2c5096526f242d2a8
Mike Johnson <72291166+enosis1@users.noreply.github.com>
2024-07-18 22:04:45 -0400

    Remove misspell diagnostics as it's deprecated

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -9,8 +9,6 @@ return {
         null_ls.builtins.formatting.prettier,
         null_ls.builtins.formatting.black,
         null_ls.builtins.formatting.isort,
-        null_ls.builtins.diagnostics.misspell, --null_ls.builtins.diagnostics.mypy,
-        --diagnostics
       },
     })
     vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
